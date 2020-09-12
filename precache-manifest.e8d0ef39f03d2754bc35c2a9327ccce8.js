79386f3bd30315446bc3bcadec968280765cfcfd
yasaratcoding <29199838+yasaratcoding@users.noreply.github.com>
2020-09-12 22:54:48 +0530

    Updates

---
+++ precache-manifest.e8d0ef39f03d2754bc35c2a9327ccce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ab68705866f794ebb5e13cca912201",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "9c5e8fd9e80403955e6a",
+    "url": "/myportfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/myportfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/myportfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c5e8fd9e80403955e6a",
+    "url": "/myportfolio/static/js/main.9118554d.chunk.js"
+  },
+  {
+    "revision": "e6a1b055e95a4a385f54",
+    "url": "/myportfolio/static/js/runtime-main.42579bc2.js"
+  }
+]);
